4defe6884b41a5be2587401dce261e9a40aa917a
Gabriela Susana Mendez Barrera <74455872+GabyArgu@users.noreply.github.com>
2023-11-06 17:18:11 -0600

    Revert "Revert "Merge pull request #30 from GabyArgu/Carias""
    This reverts commit 547169d275e36669d0a96a964877a3560a0474f7.

---
+++ Controlador/EquiposController.cs
@@ -0,0 +1,53 @@
+﻿using Modelo;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Controlador
+{
+    public class EquiposController
+    {
+        public static SqlConnection Connect_Controller()
+        {
+            return Conexion.getConnect();
+        }
+        //Atributos tb_Acta_Defuncion
+        public string codigoEquipos { get; set; }
+        public string Nombre { get; set; }
+        public int codigoUbicacion { get; set; }        
+
+        //Constructor
+        public EquiposController() { }
+
+
+        //Métodos Paquetes Entierro
+        public static DataTable CargarEquipos_Controller()
+        {
+            return ModelEquipos.CargarEquipos();
+        }
+        public DataTable CargarUbicacion_Controller(string Laboratorio, string Ubicacion)
+        {
+            return ModelEquipos.CargarUbicacion(Laboratorio, Ubicacion);
+        }
+        public static DataTable CargarUbicacion()
+        {
+            return ModelEquipos.CargarUbicacion();
+        }
+        public bool AgregarEquipo()
+        {
+            return ModelEquipos.AgregarEquipo(Nombre, codigoUbicacion);
+        }
+        public bool ActualizarEquipo()
+        {
+            return ModelEquipos.ActualizaEquipo(codigoEquipos, Nombre, codigoUbicacion);
+        }
+        public bool EliminarEquipo()
+        {
+            return ModelEquipos.EliminarEquipo(codigoEquipos);
+        }
+    }
+}
